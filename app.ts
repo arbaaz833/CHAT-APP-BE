a41feab0310866de619d27dcf2f1d49c95b69160
arbaaz833 <arbaazali031@gmail.com>
2025-04-16 18:10:39 +0500

    staus endpoint added

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import authRouter from './src/auth/auth.router';
 
 export const app = express();
@@ -6,6 +6,10 @@ export const app = express();
 app.use(express.json());
  
 app.use('/auth', authRouter);
+
+app.get('/status',(req:Request,res:Response)=>{
+    res.status(200).json({data:'All systems good.',error:null})
+})
 
 
 
