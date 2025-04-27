140c357fab31575800ca6a47ec3c6b1afeb364d7
arbaaz833 <arbaazali031@gmail.com>
2025-04-27 22:09:44 +0500

    CORS allowed

--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from 'express';
 import authRouter from './src/auth/auth.router';
 import cors from 'cors';
+//['https://nextjs-jwt-cookie-auth.vercel.app']
 
 export const app = express();
 
 app.use(express.json());
-app.use(cors({credentials:true,origin:['https://nextjs-jwt-cookie-auth.vercel.app']}))
+app.use(cors({credentials:true,origin:"*"}))
  
 app.use('/auth', authRouter);
 
