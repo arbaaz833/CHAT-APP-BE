9da270840bb865be767daf806a63ee6e8aa28ee1
arbaaz833 <arbaazali031@gmail.com>
2025-05-10 01:47:09 +0500

    initial commit

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import authRouter from './src/auth/auth.router';
 import cors from 'cors';
-//['https://nextjs-jwt-cookie-auth.vercel.app']
+
 
 export const app = express();
 
